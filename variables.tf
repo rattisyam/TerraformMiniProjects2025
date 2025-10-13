e1677d7c44491a185169b264cbfa20c64f0bf333
rattisyam <syam098@gmail.com>
2025-10-13 21:05:03 +0530

    added all terraform files

---
+++ variables.tf
@@ -0,0 +1,56 @@
+variable "resource_group_name" {
+  description = "The name of the resource group in which to create the resources."
+  type        = string
+  default     = "myResourceGroup1"
+
+}
+
+variable "location" {
+  description = "The Azure region where the resource group will be created."
+  type        = string
+  default     = "canadacentral"
+
+}
+
+variable "vnet_name" {
+  description = "The name of the virtual network."
+  type        = string
+  default     = "myVnet"
+
+}
+
+variable "vnet_address_space" {
+  description = "The address space for the virtual network."
+  type        = string
+  default     = "10.0.0.0/16"
+
+}
+
+variable "subnet1_name" {
+  description = "The name of the subnet."
+  type        = string
+  default     = "App_Subnet"
+
+}
+
+variable "nsg_name" {
+  description = "name of nsg"
+  type        = string
+  default     = "app_nsg"
+
+}
+
+variable "lb_pub_ip" {
+  description = "Public IP for LB to access from outside"
+  type        = string
+  default     = "app_lb_pub_ip"
+
+}
+
+variable "app_lb" {
+  description = "Application LB name"
+  type        = string
+  default     = "apploadbalancer"
+}
+
+
